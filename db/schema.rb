479507493ba403d04720998700fc4a80396e80f7
Robert Whittaker <cyclistsdismount@gmail.com>
2012-03-20 23:45:59 +0000

    Find a cart.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120114073950) do
+ActiveRecord::Schema.define(:version => 20120320234311) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
