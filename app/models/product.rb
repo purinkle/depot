2a37eef9e1df40ae528256762553ac79f058e0da
Robert Whittaker <cyclistsdismount@gmail.com>
2012-03-21 00:03:45 +0000

    Connect products to carts.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :line_items
-  before_destory :ensure_not_referenced_by_any_line_item
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true, length: {minimum: 10, message: 'please enter at least 10 characters'}
