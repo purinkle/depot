b697c439f7725db5a02b79399c30699dcaa0d90e
Robert Whittaker <cyclistsdismount@gmail.com>
2012-06-01 15:59:28 +0100

    Send confirmation emails.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,17 @@ Depot::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'localhost',
+    authentication: 'plain',
+    user_name: 'cyclistsdismount',
+    password: 'wY4cF3pRcjbfmeBIMhNp',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
